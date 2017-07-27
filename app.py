61292a0e95fdd94cb8fc4d04a623e33e86a2dcf8
technick303 <technick303@gmail.com>
2017-07-27 16:19:04 +0300

    realtime_update

--- app.py
+++ app.py
@@ -5,46 +5,56 @@ import time
 from  flask_bootstrap import Bootstrap
 from  collections import defaultdict
 
-FILE_STATUS = 'openvpn-status.log'
+FILE_STATUS = '/var/log/openvpn-status.log'
 
 app  = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+
+
+def get_data():
+    cli = s.socket(s.AF_INET,s.SOCK_STREAM)
+    cli.connect(('127.0.0.1',7777))
+    cli.recv(4096)
+    cli.send('status 1\r\n')
+    time.sleep(0.1)
+    data = ''
+    while True:
+        buff = cli.recv(4096)
+        data = data +buff
+        if len(buff) < 4096 :break
+    return data
 
 
 @app.route('/')
 def index():
     return render_template('new_index.html')
 
-#This it a comment 
+#This it a comment
 
 @app.route('/get_status_new',methods = ['GET'])
-def get_status_new():
-    with open(FILE_STATUS,'r') as f:
-        lines =  f.readlines()
-        
-        end_blck_clien_index  = lines.index('ROUTING TABLE\n')
-        end_blck_routin_index = lines.index('GLOBAL STATS\n')
+def get_new():
+        lines = get_data().split('\r\n')
+        end_blck_clien_index  = lines.index('ROUTING TABLE')
+        end_blck_routin_index = lines.index('GLOBAL STATS')
         header  =lines[2].split(',')
         users = defaultdict(str)
-        
+
         for item in lines[3:end_blck_clien_index]:
             user = dict((k.strip(),v.strip()) for (k,v) in zip(header,item.split(',')))
             users[user['Real Address']] = user
-        
+
         header =lines[end_blck_clien_index+1].split(',')
-    
+
         for item in lines[end_blck_clien_index+2:end_blck_routin_index]:
              user = dict((k.strip(),v.strip()) for (k,v) in zip(header,item.split(',')))
              users[user['Real Address']]['Virtual Address']=user['Virtual Address']
-             users[user['Real Address']]['Last Ref'] = user['Last Ref'] 
-    
-    return jsonify({'users':users})
+             users[user['Real Address']]['Last Ref'] = user['Last Ref']
 
+        return jsonify({'users':users})
 
 
 
 if __name__ =='__main__':
-	app.run(host='localhost',port=5555,debug=True)
-
-
+    app.run(host='0.0.0.0',port=5555,debug=True)
 
